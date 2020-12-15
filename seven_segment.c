3d45c418ae0944ad619d63607eb0692304ba1e6c
Maxim Uymin <m.uymin@corp.mail.ru>
2020-12-15 14:29:10 +0300

    Typo fixed

--- seven_segment.c
+++ seven_segment.c
@@ -13,9 +13,9 @@
 
 #define SEG_DATA_TO_DISPLAY_ARR_SIZE 8 /* 4 indicators + 4 addresses */
 
-static byte_t seg_data_to_diaplay[SEG_DATA_TO_DISPLAY_ARR_SIZE] =
+static byte_t seg_data_to_display[SEG_DATA_TO_DISPLAY_ARR_SIZE] =
 	{SEG_ADDR_FREQ_HI, 0, SEG_ADDR_FREQ_LO, 0, SEG_ADDR_DC_HI, 0, SEG_ADDR_DC_LO, 0};
-static size_t seg_data_to_diaplay_i = 0;
+static size_t seg_data_to_display_i = 0;
 static bool *seg_display_done = NULL;
 
 void
@@ -27,19 +27,19 @@ seg_display_sig_props_async(struct sig_props *props, bool *done)
 	int freq_khz = props->freq / 1000;
 	int dc = props->dc;
 
-	seg_data_to_diaplay[0] = SEG_ADDR_FREQ_HI;
-	seg_data_to_diaplay[1] = byte_hi(freq_khz);
-	seg_data_to_diaplay[2] = SEG_ADDR_FREQ_LO;
-	seg_data_to_diaplay[3] = byte_lo(freq_khz);
-	seg_data_to_diaplay[4] = SEG_ADDR_DC_HI;
-	seg_data_to_diaplay[5] = byte_hi(dc);
-	seg_data_to_diaplay[6] = SEG_ADDR_DC_LO;
-	seg_data_to_diaplay[7] = byte_lo(dc);
+	seg_data_to_display[0] = SEG_ADDR_FREQ_HI;
+	seg_data_to_display[1] = byte_hi(freq_khz);
+	seg_data_to_display[2] = SEG_ADDR_FREQ_LO;
+	seg_data_to_display[3] = byte_lo(freq_khz);
+	seg_data_to_display[4] = SEG_ADDR_DC_HI;
+	seg_data_to_display[5] = byte_hi(dc);
+	seg_data_to_display[6] = SEG_ADDR_DC_LO;
+	seg_data_to_display[7] = byte_lo(dc);
 
 	seg_display_done = done;
 
-	spi_write_byte_async(seg_data_to_diaplay[0]);
-	seg_data_to_diaplay_i = 1;
+	spi_write_byte_async(seg_data_to_display[0]);
+	seg_data_to_display_i = 1;
 }
 
 void
@@ -51,12 +51,12 @@ seg_display_sig_props_async_continue()
 		return; // yield
 	}
 
-	if (seg_data_to_diaplay_i >= SEG_DATA_TO_DISPLAY_ARR_SIZE) {
+	if (seg_data_to_display_i >= SEG_DATA_TO_DISPLAY_ARR_SIZE) {
 		*seg_display_done = true;
 		seg_display_done = NULL;
 		return;
 	}
 
-	spi_write_byte_async(seg_data_to_diaplay[seg_data_to_diaplay_i]);
-	seg_data_to_diaplay_i++;
+	spi_write_byte_async(seg_data_to_display[seg_data_to_display_i]);
+	seg_data_to_display_i++;
 }
